40475ef79a8501af426dc13b877cefb6e8f46b50
Carllos166 <carlos_rj18@hotmail.com>
2024-09-27 13:42:01 -0300

    fix: checkout path ./terraform

--- terraform/main/modules/compute_instance/outputs.tf
+++ terraform/main/modules/compute_instance/outputs.tf
@@ -1,3 +1,20 @@
 output "instance_name" {
   value = google_compute_instance.vm_instance.name
 }
+
+output " machine_type" {
+  value = google_compute_instance.vm_instance.machine_type
+}
+
+output "zone" {
+  value = google_compute_instance.vm_instance.zone
+}
+
+output "image" {
+  value = google_compute_instance.vm_instance.image
+}
+
+output "network" {
+  value = google_compute_instance.vm_instance.network
+}
+
