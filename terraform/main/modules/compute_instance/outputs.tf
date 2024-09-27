af0abc0b43ec5471bd2bb0ff2532ad3a7f808229
Carllos166 <carlos_rj18@hotmail.com>
2024-09-27 13:57:54 -0300

    fix: module export e remoção path ./terraform ci

--- terraform/main/modules/compute_instance/outputs.tf
+++ terraform/main/modules/compute_instance/outputs.tf
@@ -2,19 +2,19 @@ output "instance_name" {
   value = google_compute_instance.vm_instance.name
 }
 
-output " machine_type" {
-  value = google_compute_instance.vm_instance.machine_type
-}
-
-output "zone" {
+output "instance_zone" {
   value = google_compute_instance.vm_instance.zone
 }
 
-output "image" {
-  value = google_compute_instance.vm_instance.image
+output "instance_machine_type" {
+  value = google_compute_instance.vm_instance.machine_type
 }
 
-output "network" {
-  value = google_compute_instance.vm_instance.network
+output "instance_network" {
+  value = google_compute_instance.vm_instance.network_interface[0].network
+}
+
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
 
