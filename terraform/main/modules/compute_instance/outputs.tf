6e86db3900daeca96a26cacd143dee17c7414116
Carllos166 <carlos_rj18@hotmail.com>
2024-09-28 02:14:44 -0300

    new: removendo outputs

--- terraform/main/modules/compute_instance/outputs.tf
+++ terraform/main/modules/compute_instance/outputs.tf
@@ -10,11 +10,4 @@ output "instance_machine_type" {
   value = google_compute_instance.vm_instance.machine_type
 }
 
-output "instance_network" {
-  value = google_compute_instance.vm_instance.network
-}
-
-output "image" {
-  value = google_compute_instance.vm_instance.image
-}
 
