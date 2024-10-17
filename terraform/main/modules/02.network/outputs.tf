901ab9972457d91bfe7903e570a9bb8bbbfb8752
Carlos Henrique Gomes <75595248+Carllos166@users.noreply.github.com>
2024-10-17 15:28:05 -0300

    Update outputs.tf

--- terraform/main/modules/02.network/outputs.tf
+++ terraform/main/modules/02.network/outputs.tf
@@ -2,6 +2,6 @@ output "network" {
   value = google_compute_network.vpc_network.self_link
 }
 
-output "subnet" {
-  value = data.google_compute_subnetwork.default[0].name
-}
+#output "subnet" {
+#  value = data.google_compute_subnetwork.default[0].name
+#}
